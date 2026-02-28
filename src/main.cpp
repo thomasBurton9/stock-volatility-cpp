62dc19c3049fbee90dbd8f0908ebd11f13b8c587
Thomas Burton <thomasburton9@protonmail.com>
2026-02-28 21:15:18 +1100

    Added a main loop for user interaction and a function to help in parsing ints

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,41 @@
-#include <vector>
-#include <print>
+#include <charconv>
 #include <expected>
 #include <iostream>
+#include <optional>
+#include <print>
 #include <string>
+#include <vector>
 
 #include "csvUtils.hpp"
 #include "volatilityUtils.hpp"
 
-using std::vector;
-using std::println;
-using std::print;
-using std::expected;
 using std::cin;
-using std::unexpected;
-using std::string;
+using std::errc;
+using std::expected;
+using std::from_chars;
 using std::getline;
-using std::ws;
+using std::nullopt;
+using std::optional;
+using std::print;
+using std::println;
+using std::string;
+using std::unexpected;
+using std::vector;
 
-enum class InputError { InvalidInt };
 enum class StringError { EmptyInput };
 
-expected<int, InputError> get_int() {
-    int user_input;
-    if (!(cin >> user_input)) {
-        return unexpected(InputError::InvalidInt);
+optional<int> tryParseInt(const string &s) {
+    int x;
+    auto [ptr, error_code] = from_chars(s.data(), s.data() + s.size(), x);
+    if (error_code == errc{} && ptr == s.data() + s.size()) {
+        return x;
     }
-    return user_input;
+    return nullopt;
 }
 
 expected<string, StringError> get_string() {
     string buffer;
-    getline(cin >> ws, buffer);
+    getline(cin, buffer);
 
     if (buffer.empty()) {
         return unexpected(StringError::EmptyInput);
@@ -38,41 +43,89 @@ expected<string, StringError> get_string() {
 
     return buffer;
 }
+bool get_user_values(int &time_period, int &vol_time_period, string &ticker) {
+    bool success = false;
+    while (true) {
+        print("Please enter the time period in days: ");
 
+        auto user_str_entry = get_string();
+
+        if (!user_str_entry) {
+            println("You need to enter something. Enter 'q' to quit or a "
+                    "number for the time period.");
+            continue;
+        }
+        if (user_str_entry == "q") {
+            return success;
+        }
+
+        if (auto x = tryParseInt(*user_str_entry)) {
+            time_period = *x;
+            break;
+        } else {
+            println("That is not a number. Please enter 'q' to quit or a "
+                    "number for the time period. ");
+        }
+    }
+    while (true) {
+        print("Please enter the volatility time period in days: ");
+
+        auto user_str_entry = get_string();
+
+        if (!user_str_entry) {
+            println("You need to enter something. Enter 'q' to quit or a "
+                    "number for the volatility time period.");
+            continue;
+        }
+        if (user_str_entry == "q") {
+            return success;
+        }
+
+        if (auto x = tryParseInt(*user_str_entry)) {
+            vol_time_period = *x;
+            break;
+        } else {
+            println("That is not a number. Please enter 'q' to quit or a "
+                    "number for the volatility time period. ");
+        }
+    }
+    while (true) {
+        print("Please enter the ticker you want: ");
+        auto user_str_entry = get_string();
+
+        if (!user_str_entry) {
+            println("You need to enter something. Enter 'q' to quit or a "
+                    "ticker symbol.");
+            continue;
+        }
+        ticker = *user_str_entry;
+        success = true;
+        return success;
+    }
+}
 int main() {
-    // Change to proper logic - a loop with proper exits
-    print("Please enter the time period: ");
+    println("Welcome to the stock volatility grapher, enter 'q' at any time to "
+            "quit");
+    while (true) {
+        int time_period, vol_time_period;
+        string ticker;
 
-    auto time_period = get_int();
+        if (!get_user_values(time_period, vol_time_period, ticker)) {
+            return 0;
+        }
+        auto stockData = returnCsvStockData(ticker);
 
-    if (!time_period) {
-        return 1; 
-    }
-    print("Please enter the volatility time period: ");
+        if (!stockData) {
+            return 1;
+        }
 
-    auto vol_time_period = get_int();
+        vector<double> volatilityStockData =
+            get_moving_volatility(vol_time_period, *stockData);
 
-    if (!vol_time_period) {
-        return 1;
-    }
-
-    print("Please enter the ticker you want: ");
-    auto ticker = get_string();
-
-    if (!ticker) {
-        return 1;
-    }
-
-    auto stockData = returnCsvStockData(*ticker);
-
-    if (!stockData) {
-        return 1;
-    }
-
-    vector<double> volatilityStockData = get_moving_volatility(*vol_time_period, *stockData);
-
-    for (int i = 0; i < volatilityStockData.size(); i++) {
-        print("{:.3f} ", volatilityStockData[i]);
+        for (int i = 0; i < volatilityStockData.size(); i++) {
+            print("{:.3f} ", volatilityStockData[i]);
+        }
+        println("");
     }
 
     return 0;
